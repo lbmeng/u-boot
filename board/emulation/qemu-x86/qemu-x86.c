f053da8b273caa9bff96c86d61dad4a795af277c
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2024-03-08 12:23:46 +0800

    x86: qemu: Enable ramfb by default
    Now that we have everything in place to support ramfb, let's wire it up
    by default in the x86 QEMU targets. That way, we can use ramfb graphical
    console instead of the default by passing -vga none -device ramfb to the
    QEMU command line.
    
    Also increase SYS_MALLOC_F_LEN for QEMU x86_64 to be the same as its SPL
    counterpart, because we're running out of alloc space in pre-reloc stage
    with ramfb enabled.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>
    [Bin: Remove <common.h>, and rebase on u-boot/master]
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>

---
+++ board/emulation/qemu-x86/qemu-x86.c
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2015, Bin Meng <bmeng.cn@gmail.com>
+ */
+
+#include <init.h>
+#include <log.h>
+#include <qfw.h>
+
+int board_early_init_f(void)
+{
+	struct udevice *dev;
+	int ret;
+
+	/*
+	 * Make sure we enumerate the QEMU Firmware device to bind ramfb
+	 * so video_reserve() can reserve memory for it.
+	 */
+	if (IS_ENABLED(CONFIG_QFW)) {
+		ret = qfw_get_dev(&dev);
+		if (ret) {
+			log_err("Failed to get QEMU FW device: %d\n", ret);
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
+int board_early_init_r(void)
+{
+	struct udevice *dev;
+	int ret;
+
+	/*
+	 * Make sure we enumerate the QEMU Firmware device to find ramfb
+	 * before console_init.
+	 */
+	if (IS_ENABLED(CONFIG_QFW)) {
+		ret = qfw_get_dev(&dev);
+		if (ret) {
+			log_err("Failed to get QEMU FW device: %d\n", ret);
+			return ret;
+		}
+	}
+
+	return 0;
+}
